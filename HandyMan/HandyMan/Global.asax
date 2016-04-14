9e228ab614dda8a4ff811e685603831cb452264e
thanos <atha0043@edu.eal.dk>
2016-04-14 11:55:39 +0200

    Thanos

---
+++ HandyMan/HandyMan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HandyMan.MvcApplication" Language="C#" %>
---
+++ HandyMan/HandyMan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HandyMan.Startup))]
+namespace HandyMan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
